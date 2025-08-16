afaadf219b7c7b86e63a6c64a699f95359395b4c
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-08-16 08:10:42 +0700

    add: Create LayoutDetail component with dynamic routing and component mapping

---
+++ chat-app-frontend/src/layouts/LayoutDetail.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tyn-root">
+    <component
+      v-for="name in route.meta.componentsInRoot || []"
+      :is="componentMap[name]"
+      :key="name"
+    />
+    <router-view />
+    <component
+      v-for="name in route.meta.componentsChatorFooter || []"
+      :is="componentMap[name]"
+      :key="name"
+    />
+  </div>
+
+  <component
+    v-for="name in route.meta.componentsOutsideRoot || []"
+    :is="componentMap[name]"
+    :key="name"
+  />
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import ChatBox from "@/components/ChatBox.vue";
+import ModalCall from "@/components/ModalCall.vue";
+import ModalVideoCall from "@/components/ModalVideoCall.vue";
+import ModalContact from "@/components/ModalContact.vue";
+import ModalMuteOpt from "@/components/ModalMuteOpt.vue";
+import ModalNewChat from "@/components/ModalNewChat.vue";
+import ModalDeleteChat from "@/components/ModalDeleteChat.vue";
+
+import { useRoute } from "vue-router";
+
+export default {
+  name: "LayoutDetail",
+  setup() {
+    const route = useRoute();
+
+    const componentMap = {
+      Header,
+      Footer,
+      ChatBox,
+      ModalCall,
+      ModalVideoCall,
+      ModalContact,
+      ModalMuteOpt,
+      ModalNewChat,
+      ModalDeleteChat
+    };
+
+    return { route, componentMap };
+  },
+};
+</script>

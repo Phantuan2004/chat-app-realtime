1eef3d95013f2a54d0776b37e50965621129b9f0
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-08-21 04:58:06 +0700

    add library primevue

--- chat-app-frontend/src/main.js
+++ chat-app-frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers/index'
+import ToastService from 'primevue/toastservice'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ToastService).mount('#app')

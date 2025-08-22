956bef2c39554a85ac7b4df2501d60a5caff447b
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-08-22 08:12:47 +0700

    add library toast notify

--- chat-app-frontend/src/main.js
+++ chat-app-frontend/src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 import router from './routers/index'
-import ToastService from 'primevue/toastservice'
 
-createApp(App).use(router).use(ToastService).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    }
+})
+app.use(ToastService)
+app.use(router)
+app.mount('#app')

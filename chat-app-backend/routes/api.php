fd7b1609026fa943bd659176dfe83b573a49f86e
phantuan2004 <phanhoangtuan21072004@gmail.com>
2025-08-23 16:34:42 +0700

    reactor: add prefix group route users. And add route conversation, messages

--- chat-app-backend/routes/api.php
+++ chat-app-backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ConversationController;
+use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,15 +18,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+// Route CRUD user
+Route::prefix('/users')->group(function () {
+    Route::get('', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
 
+// Route auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:api', 'check.token.expiry'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Route conversation 
+Route::middleware(['auth:api', 'check.token.expiry'])->prefix('/conversations')->group(function() {
+    Route::get('', [ConversationController::class, 'index']);
+    Route::get('/{id}', [ConversationController::class, 'show']);
+    Route::post('/', [ConversationController::class, 'store']);
+    Route::put('/{id}', [ConversationController::class, 'update']);
+    Route::delete('/{id}', [ConversationController::class, 'destroy']);
+    Route::post('/{id}/leave', [ConversationController::class, 'leave']);
+});
+
+// Route message
+Route::middleware(['auth:api', 'check.token.expiry'])->prefix('/messages')->group(function() {
+    Route::get('/{conversationId}', [MessageController::class, 'index']);
+    Route::post('/{conversationId}', [MessageController::class, 'store']);
+    Route::delete('/{messageId}', [MessageController::class, 'destroy']);
+    Route::post('/{messageId}/recall', [MessageController::class, 'recall']);
+    Route::post('/{messageId}/markAsRead', [MessageController::class, 'markAsRead']);
 });
